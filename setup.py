4dae7268922db8409c9772f742c360ea7231ff0b
reyoung <reyoung@126.com>
2013-12-19 12:55:35 +0800

    add pypi for setuptolls

--- setup.py
+++ setup.py
@@ -1,1 +1,14 @@
+from setuptools import setup
 __author__ = 'reyoung'
+
+setup(
+    name='singleton',
+    version="0.1.0",
+    packages=['singleton'],
+    author=__author__,
+    author_email='reyoung@126.com',
+    license='MIT',
+    description='A singleton class decorator of python.',
+    keywords='singleton thread-safe',
+    url='https://github.com/reyoung/singleton'
+)
